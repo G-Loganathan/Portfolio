06e9a84709588062af4db7d58b3f397815b90b87
G-Loganathan <loganathan2624@gmail.com>
2024-02-08 10:06:52 +0530

    email integration

--- src/components/Contact/Contacts.jsx
+++ src/components/Contact/Contacts.jsx
@@ -2,21 +2,55 @@ import React from "react";
 import "../Contact/Contacts.css";
 import { Element } from "react-scroll";
 import Footer from "../Footer/Footer";
-import Swal from "sweetalert2";
+// import Swal from "sweetalert2";
+import axios from 'axios';
+import emailjs from '@emailjs/browser';
+
 
 const Contacts = () => {
-  const sendMail = () => {
-    Swal.fire({
-      title: "Sended!",
-      text: "Thank you for your Interest, I'll get back to you soon!",
-      icon: "success",
-    });
-  };
+
+  // Swal.fire({
+  //   title: "Sended!",
+  //   text: "Thank you for your Interest, I'll get back to you soon!",
+  //   icon: "success",
+  // });
+
+  const [name, setName] = React.useState();
+  const [email, setEmail] = React.useState();
+  // const [subject, setSubject] = React.useState();
+  const [message, setMessage] = React.useState();
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const serviceId = 'service_k0vkk9f';
+    const templateId = 'template_z9gox9g';
+    const publicKey = '1WGI8Sc2aG5v6RS4e';
+
+    const templateParams = {
+        from_name: name,
+        from_email: email,
+        to_name: 'Loganathan',
+        // subject: subject,
+        message: message
+    };
+
+    emailjs.send(serviceId, templateId, templateParams, publicKey)
+    .then((response) => {
+      console.log('Email sent successfully', response);
+      setName('');
+      setEmail('');
+      setMessage('');
+    })
+    .catch((error) => {
+      console.log('Error sending email:', error)
+    })
+  }
 
   return (
     <Element name="contact">
       <div className="contacts__container">
-        <form action="">
+        <form action="" onClick={handleSubmit()} >
           <div className="contacts__header">
             <div className="contacts-title">
               <p>CONTACT</p>
@@ -30,40 +64,40 @@ const Contacts = () => {
             <input
               className="name-input"
               type="text"
-              name=""
-              id=""
               placeholder="Enter your Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
             <input
               className="email-input"
               type="text"
-              name=""
-              id=""
               placeholder="Enter your Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
 
           <div className="inputGroup__two">
-            <input
-              className="subject-input"
-              type="text"
-              name=""
-              id=""
-              placeholder="Enter Subject"
-            />
+              {/* <input
+                className="subject-input"
+                type="text"
+                placeholder="Enter Subject"
+                // value={subject}
+                // onChange={(e) => setSubject(e.target.value)}
+              /> */}
             <textarea
               className="messege-input"
-              name=""
-              id=""
               cols="30"
               rows="10"
-              placeholder="Enter Messege"
+              placeholder="Enter Message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
             ></textarea>
           </div>
 
           <div className="contacts__btn">
-            <button onClick={() => sendMail()} type="button">
-              Send Messege
+            <button type="button">
+              Send Message
             </button>
           </div>
         </form>
@@ -73,5 +107,7 @@ const Contacts = () => {
     </Element>
   );
 };
+
+
 
 export default Contacts;
